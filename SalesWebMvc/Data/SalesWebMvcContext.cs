064c57745878956a5472311b01239e50af6c7866
gutakeuchi <gustavo.takeuchi87@gmail.com>
2023-06-03 23:26:30 -0300

    MySQL adaptation and first migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -14,6 +14,6 @@ namespace SalesWebMvc.Data
         {
         }
 
-        public DbSet<SalesWebMvc.Models.Department> Department { get; set; } = default!;
+        public DbSet<Department> Department { get; set; } = default!;
     }
 }
