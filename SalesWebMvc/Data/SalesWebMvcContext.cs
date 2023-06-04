169ba25964f421701cd4430d05102184938ad408
gutakeuchi <gustavo.takeuchi87@gmail.com>
2023-06-04 00:16:53 -0300

    Other entities and second migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -14,6 +14,14 @@ namespace SalesWebMvc.Data
         {
         }
 
-        public DbSet<Department> Department { get; set; } = default!;
+        public DbSet<Department> Department { get; set; }
+
+        public DbSet<Seller> Seller { get; set; }
+
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+
+
+
+
     }
 }
